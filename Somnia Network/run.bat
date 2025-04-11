1f143e0c214518979e64eac9c46015f67f915f1d
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-12 04:58:57 +0700

    Update run.bat

--- Somnia Network/run.bat
+++ Somnia Network/run.bat
@@ -18,11 +18,15 @@ title "Somnia network bot by @meomundep"
                 echo   "referralCode": ["4BB70DFF"],
                 echo   "autoReferrals": true,
                 echo   "setUsername": true,
+                echo   "swapToken": true,
+                echo   "slippage": 2.5,
+                echo   "amountOfPingForPong": 0.01,
+                echo   "amountOfPongForPing": 0.01,
                 echo   "faucetToken": true,
-                echo   "sendTokenTo": [
-                echo     "0xe81E898F01Bc49B4A567628A6876A68ab1Bc4646",
-                echo     "0x5A0C7eB124B3ADE21D171BBBa97E5899e2fEdBE1",
-                echo     "0x445456AABE6E892a1280b98754b0f6AF10440c8a"
+                echo   "mintToken": true,
+                echo   "toNewWallet": true,
+                echo   "sendAndMintTokenTo": [
+                echo     "add_your_addresses_here"
                 echo   ]
                 echo }
             ) > %%F
